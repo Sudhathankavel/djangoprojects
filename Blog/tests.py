c85e3c09010d3b8f288d0c164429d6614360a481
sudhathankavel <sudhathankavel88@gmail.com>
2018-09-11 16:14:54 +0530

    Asserted the field errors instead of just checking form is valid and changed the variable name

--- Blog/tests.py
+++ Blog/tests.py
@@ -11,20 +11,24 @@ class FormFieldTest(TestCase):
     def test_fields_for_ten_character(self):
         form_data = {'title': 'test', 'content': 'content'}
         form = BlogForm(data=form_data)
-        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 2)
+        self.assertEquals(form.errors['title'], ['Ensure this value has at least 10 characters (it has 4).'])
+        self.assertEquals(form.errors['content'], ['Ensure this value has at least 10 characters (it has 7).'])
 
     def test_to_check_content_greater_than_title(self):
         data = {'title': 'testTitle With ten Characters', 'content': 'test content with TC'}
         form = BlogForm(data=data)
-        self.assertFalse(form.is_valid())
+        # import pdb;pdb.set_trace()
+        self.assertEqual(len(form.errors), 1)
+        self.assertEquals(form.errors['__all__'], ['content should be longer than title.'])
 
 
 class FormBlogTest(TestCase):
     def test_blog_form(self):
         client = Client(enforce_csrf_checks=True)
-        request = self.client.post(reverse('Blog:homepage'), {'title': 'test Title With ten Characters', 'content': 'test content with TC'})
+        response = self.client.post(reverse('Blog:homepage'), {'title': 'test Title With ten Characters', 'content': 'test content with TC'})
         error_data = 'content should be longer than title.'
-        self.assertContains(request, error_data, status_code=200)
+        self.assertContains(response, error_data, status_code=200)
 
 
 

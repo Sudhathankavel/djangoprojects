8e5494a87f918a26e8dd3c7a8eaf34e8cac77777
sudhathankavel <sudhathankavel88@gmail.com>
2018-09-27 16:23:17 +0530

    fixes- used reverse_lazy

--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic import CreateView,TemplateView
+from django.views.generic import CreateView,TemplateView, UpdateView
 from .forms import BlogForm, LoginForm, SignUpFrom
 from .models import Blog
 from django.urls import reverse, reverse_lazy
 import uuid
+from django.http import Http404
 from django.contrib import messages
 from django.utils.safestring import mark_safe
 from bs4 import BeautifulSoup
@@ -14,7 +15,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class MyLoginRequiredMixin(LoginRequiredMixin):
-    login_url = '/login/'
+    login_url = reverse_lazy('Blog:login')
     redirect_field_name = 'next'
 
 
@@ -33,14 +34,12 @@ class HomePage(View):
                 instance.secret_key = uuid.uuid4().hex[:6].upper()
                 instance.save()
                 url_path = reverse('Blog:homepage', kwargs={'id': instance.id, 'secret_key': instance.secret_key})
-                messages.success(request, mark_safe("<a href='{url_path}'>{url_path}k</a>".format(url_path=url_path)))
-                return redirect("Blog:Content", id=instance.id)
             else:
                 instance.author = request.user
                 instance.save()
                 url_path = reverse('Blog:homepage', kwargs={'id': instance.id})
-                messages.success(request, mark_safe("<a href='{url_path}'>{url_path}</a>".format(url_path=url_path)))
-                return redirect("Blog:Content", id=instance.id)
+            messages.success(request, mark_safe("<a href='{url_path}'>{url_path}</a>".format(url_path=url_path)))
+            return redirect("Blog:Content", id=instance.id)
         return render(request, "Blog/homepage.html", {'form': form, 'ctx': self.ctx})
 
 
@@ -61,28 +60,27 @@ class ContentPage(View):
 class EditPage(View):
     ctx = {'id': 'edit_post', 'button': 'Edit and Save'}
 
-    def get(self, request, id, secret_key=None):
-        if secret_key is None:
-            blog = get_object_or_404(Blog, id=id)
-            form = BlogForm(instance=blog)
+    def dispatch(self, request, *args, **kwargs):
+        if self.kwargs.get('secret_key'):
+            self.blog = get_object_or_404(Blog, id=self.kwargs.get('id'), secret_key=self.kwargs.get('secret_key'))
         else:
-            blog = get_object_or_404(Blog, id=id, secret_key=secret_key)
-            form = BlogForm(instance=blog)
-        return render(request, "Blog/homepage.html", {'form': form, 'ctx': self.ctx})
+            self.blog = get_object_or_404(Blog, id=self.kwargs.get('id'))
+            if not self.blog.author == request.user:
+                raise Http404()
+        return super(EditPage, self).dispatch(request, *args, **kwargs)
 
-    def post(self, request, id, secret_key=None):
-        if secret_key is None:
-            blog = get_object_or_404(Blog, id=id)
-            form = BlogForm(request.POST or None, instance=blog)
-        else:
-            blog = get_object_or_404(Blog, id=id, secret_key=secret_key)
-            form = BlogForm(request.POST or None, instance=blog)
+    def get(self, *args, **kwargs):
+        form = BlogForm(instance=self.blog)
+        return render(self.request, "Blog/homepage.html", {'form': form, 'ctx': self.ctx})
+
+    def post(self, *args, **kwargs):
+        form = BlogForm(self.request.POST or None, instance=self.blog)
         if form.is_valid():
             instance = form.save()
             instance.save()
-            messages.success(request, "Your post has been successfully Updated!!!")
+            messages.success(self.request, "Your post has been successfully Updated!!!")
             return redirect("Blog:Content", id=instance.id)
-        return render(request, "Blog/homepage.html", {'form': form, 'ctx': self.ctx})
+        return render(self.request, "Blog/homepage.html", {'form': form, 'ctx': self.ctx})
 
 
 class SignupView(CreateView):
@@ -105,16 +103,13 @@ class LoginView(View):
             username = request.POST['username']
             password = request.POST['password']
             user = User.objects.get(username=username)
-            if user is not None:
-                if user.check_password(password):
-                        if user.is_active:
-                            login(request, user)
-                            return redirect("Blog:homepage")
+            if user.check_password(password) and user.is_active:
+                login(request, user)
+                return redirect("Blog:homepage")
         return render(request, "Blog/login.html", {"form": form, 'ctx': self.ctx})
 
 
 class LogoutView(View):
-
     def post(self, request):
         logout(request)
         return redirect("Blog:homepage")
@@ -127,5 +122,3 @@ class ArticlesView(MyLoginRequiredMixin, TemplateView):
         context = super(ArticlesView, self).get_context_data(**kwargs)
         context['articles'] = Blog.objects.filter(author=self.request.user)
         return context
-
-
